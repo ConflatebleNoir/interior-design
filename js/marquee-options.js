9e87cf24dae921312078ca738fb2087ba991ea34
Potapenko Max <mpotapenko.mail@gmail.com>
2022-03-17 10:42:56 +0300

    marquee implementation (updated)

--- js/marquee-options.js
+++ js/marquee-options.js
@@ -1,37 +1,53 @@
 $(function () {
     $('.ticker__first').marquee({
-        duration: 10000,
+        duration: 5000,
         startVisible: true,
         duplicated: true,
         direction: 'down',
         easing: 'ease',
-        speed: 100
-    }),
+        speed: 30,
+        gap: 10,
+        delayBeforStart: 0,
+        pauseOnCycle: true,
+        pauseOnHover: true
+    })
 
-        $('.ticker__second').marquee({
-            duration: 10000,
-            startVisible: true,
-            duplicated: true,
-            direction: 'up',
-            easing: 'ease',
-            speed: 100
-        }),
+    $('.ticker__second').marquee({
+        duration: 5000,
+        startVisible: true,
+        duplicated: true,
+        direction: 'up',
+        easing: 'ease',
+        speed: 30,
+        gap: 10,
+        delayBeforStart: 0,
+        pauseOnCycle: true,
+        pauseOnHover: true
+    })
 
-        $('.ticker__third').marquee({
-            duration: 10000,
-            startVisible: true,
-            duplicated: true,
-            direction: 'down',
-            easing: 'ease',
-            speed: 100
-        }),
+    $('.ticker__third').marquee({
+        duration: 5000,
+        startVisible: true,
+        duplicated: true,
+        direction: 'down',
+        easing: 'ease',
+        speed: 30,
+        gap: 10,
+        delayBeforStart: 0,
+        pauseOnCycle: true,
+        pauseOnHover: true
+    })
 
-        $('.ticker__fourth').marquee({
-            duration: 10000,
-            startVisible: true,
-            duplicated: true,
-            direction: 'up',
-            easing: 'ease',
-            speed: 100
-        });
+    $('.ticker__fourth').marquee({
+        duration: 5000,
+        startVisible: true,
+        duplicated: true,
+        direction: 'up',
+        easing: 'ease',
+        speed: 30,
+        gap: 10,
+        delayBeforStart: 0,
+        pauseOnCycle: true,
+        pauseOnHover: true
+    });
 });
